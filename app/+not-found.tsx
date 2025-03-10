269622d70e92cca0ab90dc563ad31855176ab848
MigoiArmando <ashurasyntax@gmail.com>
2025-03-10 17:32:00 +0800

    BL-17-0.4.16
    Added the 404screen (+not-found.tsx)

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,18 +1,51 @@
 import { Link, Stack } from "expo-router";
 import { StyleSheet } from "react-native";
+import {
+    useFonts,
+    Lexend_400Regular,
+    Lexend_600SemiBold,
+} from "@expo-google-fonts/lexend";
 
 import React from "react";
 import { View, Text } from "../components/expo/Themed";
+import NotFoundImage from "@/assets/images/404notfound.svg";
 
 export default function NotFoundScreen() {
+    const [fontsLoaded] = useFonts({
+        Lexend_400Regular,
+        Lexend_600SemiBold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <>
-            <Stack.Screen options={{ title: "Oops!" }} />
+            <Stack.Screen
+                options={{
+                    title: "Go Back",
+                    headerTitleStyle: {
+                        fontFamily: "Lexend_600SemiBold",
+                        fontSize: 16,
+                        color: "#0075B2",
+                    },
+                    headerTitleAlign: "center",
+                }}
+            />
             <View style={styles.container}>
-                <Text style={styles.title}>This screen doesn't exist.</Text>
+                <NotFoundImage width={300} height={200} />
 
                 <Link href="/" style={styles.link}>
-                    <Text style={styles.linkText}>Go to home screen!</Text>
+                    <Text
+                        style={[
+                            styles.linkText,
+                            { fontFamily: "Lexend_400Regular" },
+                        ]}
+                    >
+                        We're working hard to bring you this feature. Stay tuned
+                        for updates!
+                    </Text>
                 </Link>
             </View>
         </>
@@ -35,7 +68,8 @@ const styles = StyleSheet.create({
         paddingVertical: 15,
     },
     linkText: {
-        fontSize: 14,
-        color: "#2e78b7",
+        fontSize: 16,
+        color: "#2B3854",
+        textAlign: "center",
     },
 });
