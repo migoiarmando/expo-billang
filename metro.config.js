5dc5cf938ac08d22b405f21ca686514f77b41f2e
Jeck <zrkeimperial@gmail.com>
2025-03-15 08:56:42 +0800

    ver0.4.22 minor fixes extended configuration for expo-asset

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,15 @@
+/* --------------------------------------------------------------------------------------------------------------
+
+    Last edited: 
+        John Bicierro [March 15, 2025]
+
+    Company: github.com/codekada
+    Project: github.com/jkbicierro/expo-billang
+
+-------------------------------------------------------------------------------------------------------------- */
+
+const { withNativeWind } = require("nativewind/metro");
+
 /* eslint-env node */
 const { getDefaultConfig } = require("expo/metro-config");
 
@@ -5,13 +17,14 @@ const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
 
 config.transformer = {
+    ...config.transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
 };
+
 config.resolver = {
+    ...config.resolver,
     assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
     sourceExts: [...config.resolver.sourceExts, "svg"],
 };
-
-const { withNativeWind } = require("nativewind/metro"); // Nativewind
 
 module.exports = withNativeWind(config, { input: "./global.css" });
