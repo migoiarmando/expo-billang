e844771fe30dc1ea5c9184a2b4310089cac3c456
Peter <122733274+romarcastro@users.noreply.github.com>
2025-04-23 21:15:31 +0800

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -66,7 +66,7 @@ export default function HomeScreen() {
             await db.insert(user_tb).values([
                 {
                     name: "",
-                    onboarding: false,
+                    onboarding: true,
                 },
             ]);
 
